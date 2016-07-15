a0a2ce5385fe50dd66599dfb8852df7c99d1a371
Eduardo Cardoso <eduardo@ckl.io>
2016-07-15 17:29:38 -0300

    Add refresh method

--- spotify_token_swap.py
+++ spotify_token_swap.py
@@ -3,43 +3,7 @@ from cherrypy import tools
 import simplejson as json
 import requests
 import os
-
-'''
-    author: @plamere
-    date: 3/30/14
-
-    This is an example token swap service written in python / cherrypy
-    This is required by the Spotify iOS SDK to authenticate a user.
-
-    The service relies on the cherrypy and requests libaries to be installed:
-
-    % pip install cherrypy
-    % pip install requests
-
-    To run the service, enter your client ID, secret and callback URL
-    below and run the program with:
-
-    % python spotify_token_swap.py
-
-    IMPORTANT: You will get authorization failures if you don't insert
-    your own client credentials below.
-
-    Once the service is running, pass the public URI to it (such as
-    http://localhost:9020/swap) to the token swap method in the
-    Spotify iOS SDK:
-
-     NSURL *swapServiceURL = 
-        [NSURL # urlWithString:@"http://localhost:9020/swap"];
-    
-        -[SPAuth handleAuthCallbackWithTriggeredAuthURL:url
-               tokenSwapServiceEndpointAtURL:swapServiceURL callback:callback];
-
-Note: For this beta 1 release of the iOS SDK Spotify provides the
-above beta values you can use in your Token Exchange Service code; later,
-these values will be invalidated and will need to be replaced by your
-own unique values.
-
-'''
+import base64
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', 'production')
 
@@ -49,27 +13,39 @@ CALLBACK_URL = os.environ.get('CALLBACK_URL')
 
 SPOTIFY_TOKEN_URL = os.environ.get('SPOTIFY_TOKEN_URL', 'https://accounts.spotify.com/api/token')
 
+AUTH_HEADER = 'Basic %s' % base64.b64encode('%s:%s' % (CLIENT_ID, CLIENT_SECRET))
 
 class SpotifyTokenSwap(object):
     @cherrypy.expose
     @tools.json_out()
     def swap(self, code=None):
+        headers = {
+            'Authorization': AUTH_HEADER
+        }
         params = {
             'grant_type': 'authorization_code',
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
             'redirect_uri': CALLBACK_URL,
             'code' : code
         }
-        r = requests.post(SPOTIFY_TOKEN_URL, params)
+        r = requests.post(SPOTIFY_TOKEN_URL, params, headers=headers)
         cherrypy.response.status = r.status_code
 
-        if ENVIRONMENT != 'production':
-            print
-            print code
-            print r.status_code
-            print r.text
-            print
+        return r.json()
+
+    @cherrypy.expose
+    @tools.json_out
+    def refresh(self, refresh_token=None):
+        headers = {
+            'Authorization': AUTH_HEADER
+        }
+        params = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token
+        }
+
+        r = requests.post(SPOTIFY_TOKEN_URL, params, headers=headers)
+        cherrypy.response.status = r.status_code
+
         return r.json()
 
 def CORS():
